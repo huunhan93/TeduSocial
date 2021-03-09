1156cb5587dc9514fc19d81b11b796ade9e2dd7d
Hồ Hữu Nhân <nhanhh.dtp@vnpt.vn>
2021-03-09 10:21:51 +0700

    commit

--- src/modules/groups/groups.route.ts
+++ src/modules/groups/groups.route.ts
@@ -4,24 +4,30 @@ import { Router } from "express";
 import CreateGroupDto from "./dtos/create_group.dto";
 import GroupController from "./groups.controller";
 
- export default class GroupRoute implements Route{
-    public path = "/api/v1/groups";
-    public router = Router();
-    public groupController = new GroupController();
-  
-    constructor() {
-      this.initializeRoutes();
-    }
-  
-    private initializeRoutes() {
-      this.router.post(
-        this.path,
-        authMiddleware,
-        validationMiddleware(CreateGroupDto, true),
-        this.groupController.createGroup
-      );
-  
-      this.router.get(this.path, this.groupController.getAllGroups);
-      
-    }
- }
+export default class GroupRoute implements Route {
+  public path = "/api/v1/groups";
+  public router = Router();
+  public groupController = new GroupController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.post(
+      this.path,
+      authMiddleware,
+      validationMiddleware(CreateGroupDto, true),
+      this.groupController.createGroup
+    );
+
+    this.router.get(this.path, this.groupController.getAllGroups);
+    this.router.put(
+      this.path + "/:id",
+      authMiddleware,
+      validationMiddleware(CreateGroupDto, true),
+      this.groupController.updateGroup
+    );
+    this.router.delete(this.path + "/:id", this.groupController.deleteGroup);
+  }
+}
--- src/modules/groups/groups.service.ts
+++ src/modules/groups/groups.service.ts
@@ -13,7 +13,7 @@ export default class GroupService {
     if (!user) throw new HttpException(400, "user id is not exist");
 
     const existingGroup = await GroupSchema.find({
-      $or: [{ name: groupDto.name }, { code: groupDto.code }]
+      $or: [{ name: groupDto.name }, { code: groupDto.code }],
     }).exec();
 
     if (existingGroup.length > 0)
@@ -35,22 +35,22 @@ export default class GroupService {
 
     const existingGroup = await GroupSchema.find({
       $and: [
-        { $or: [{ $name: groupDto.name }, { $code: groupDto.code }] },
+        { $or: [{ name: groupDto.name }, { code: groupDto.code }] },
         {
-          _id: { $ne: { groupId } },
+          _id: { $ne: groupId  },
         },
       ],
     }).exec();
-    if(existingGroup.length > 0 )
-      throw new HttpException(400, "Name or code is existed")
-    const groupFields = {...groupDto}
+    if (existingGroup.length > 0)
+      throw new HttpException(400, "Name or code is existed");
+    const groupFields = { ...groupDto };
     const updatedGroup = await GroupSchema.findOneAndUpdate(
       { _id: groupId },
-      { set: groupFields },
+      { $set: groupFields },
       { new: true }
     ).exec();
 
-    if(!updatedGroup) throw new HttpException(400, "Update is not success")
+    if (!updatedGroup) throw new HttpException(400, "Update is not success");
 
     return updatedGroup;
   }
@@ -58,5 +58,17 @@ export default class GroupService {
   public async getAllGroup(): Promise<IGroup[]> {
     const posts = await GroupSchema.find().sort({ date: -1 }).exec();
     return posts;
+  }
+
+  public async deleteGroup(groupId: string): Promise<IGroup> {
+    const group = await GroupSchema.findById(groupId).exec();
+    if (!group) throw new HttpException(400, "Group is not exists");
+
+    const deleteGroup = await GroupSchema.findOneAndDelete({
+      _id: groupId,
+    }).exec();
+    if (!deleteGroup) throw new HttpException(400, "Delete is not success");
+
+    return deleteGroup;
   }
 }
